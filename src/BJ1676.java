608621c92283befae69f43378c79364c54133b48
LeeChangGeun <echk1212@gmail.com>
2023-12-20 18:26:21 +0900

    BJ1676

---
+++ src/BJ1676.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int trailingZeros = countTrailingZeros(N);
+
+        System.out.println(trailingZeros);
+    }
+
+    private static int countTrailingZeros(int N) {
+        int count = 0;
+
+        for (int i = 5; N / i >= 1; i *= 5) {
+            count += N / i;
+        }
+
+        return count;
+    }
+}

4e127447c466fb0c340a257c6ec9539faf1ca9aa
LeeChangGeun <echk1212@gmail.com>
2024-01-03 14:54:45 +0900

    BJ11650

---
+++ src/BJ11650.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class BJ11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points[i] = new Point(x, y);
+        }
+
+        Arrays.sort(points, Comparator.comparingInt((Point p) -> p.x).thenComparingInt(p -> p.y));
+
+        for (Point point : points) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}

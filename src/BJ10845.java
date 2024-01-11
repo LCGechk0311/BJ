4c9b3d946fb81cb42c24f40fd01c19a436e723ef
LeeChangGeun <echk1212@gmail.com>
2024-01-11 20:39:02 +0900

    BJ10845

---
+++ src/BJ10845.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ10845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    int X = Integer.parseInt(st.nextToken());
+                    queue.offer(X);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.poll());
+                    }
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    System.out.println(queue.isEmpty() ? 1 : 0);
+                    break;
+                case "front":
+                    System.out.println(queue.isEmpty() ? -1 : queue.peek());
+                    break;
+                case "back":
+                    System.out.println(queue.isEmpty() ? -1 : ((LinkedList<Integer>) queue).getLast());
+                    break;
+            }
+        }
+
+        br.close();
+    }
+}

3b1581d8742b3a81f9039b367f8be6bd4f08dc9c
LeeChangGeun <echk1212@gmail.com>
2024-02-07 13:46:53 +0900

    BJ1152

--- src/BJ1152.java
+++ src/BJ1152.java
@@ -1,15 +1,13 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class BJ1152 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine().trim();
-
-        String[] words = input.split("\\s+");
-
-        System.out.println(words.length);
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        System.out.print(st.countTokens());
     }
 }

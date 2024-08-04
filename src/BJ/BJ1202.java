40b1f846de199b2bc4708970de91b0ab43e67fcb
LeeChangGeun <echk1212@gmail.com>
2024-08-04 15:27:35 +0900

    BJ1202

---
+++ src/BJ/BJ1202.java
@@ -0,0 +1,68 @@
+package BJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BJ2887 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Jewel[] jewels = new Jewel[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            jewels[i] = new Jewel(weight, value);
+        }
+
+        int[] bags = new int[K];
+        for (int i = 0; i < K; i++) {
+            bags[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(jewels, Comparator.comparingInt(jewel -> jewel.weight));
+
+        Arrays.sort(bags);
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        long totalValue = 0;
+        int jewelIndex = 0;
+
+        for (int i = 0; i < K; i++) {
+            int currentBagCapacity = bags[i];
+
+            while (jewelIndex < N && jewels[jewelIndex].weight <= currentBagCapacity) {
+                maxHeap.add(jewels[jewelIndex].value);
+                jewelIndex++;
+            }
+
+            if (!maxHeap.isEmpty()) {
+                totalValue += maxHeap.poll();
+            }
+        }
+
+        System.out.println(totalValue);
+    }
+
+    static class Jewel {
+        int weight;
+        int value;
+
+        public Jewel(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+}

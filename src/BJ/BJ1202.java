7dedafc9f7dc36e4f3a4bd4a4e92f5154fe8c9cc
LeeChangGeun <echk1212@gmail.com>
2024-08-04 21:26:26 +0900

    BJ1202

--- src/BJ/BJ1202.java
+++ src/BJ/BJ1202.java
@@ -10,6 +10,16 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BJ1202 {
+    static class Jewel {
+        int weight;
+        int value;
+
+        public Jewel(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -35,34 +45,27 @@ public class BJ1202 {
 
         Arrays.sort(bags);
 
+        // 우선순위 큐(최대 힙)를 보석의 가격 기준으로 생성
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-        long totalValue = 0;
-        int jewelIndex = 0;
+        long totalValue = 0; // 보석의 최대 가격 합을 저장할 변수
+        int jewelIndex = 0; // 보석의 인덱스를 추적할 변수
 
         for (int i = 0; i < K; i++) {
             int currentBagCapacity = bags[i];
 
+            // 현재 가방에 담을 수 있는 모든 보석을 우선순위 큐에 추가
             while (jewelIndex < N && jewels[jewelIndex].weight <= currentBagCapacity) {
                 maxHeap.add(jewels[jewelIndex].value);
                 jewelIndex++;
             }
 
+            // 현재 가방에 담을 수 있는 가장 값비싼 보석을 선택
             if (!maxHeap.isEmpty()) {
-                totalValue += maxHeap.poll();
+                totalValue += maxHeap.poll(); // 가장 값비싼 보석을 totalValue에 추가
             }
         }
 
         System.out.println(totalValue);
-    }
-
-    static class Jewel {
-        int weight;
-        int value;
-
-        public Jewel(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
-        }
     }
 }
